19844179e494bfcda2fded864c25a99fa44155a7
C.J. Edwards <cedwardsprovalus@gmail.com>
2018-12-25 12:43:38 -0600

    Project

---
+++ LeagueManagerPost/Controllers/GamesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LeagueManagerPost.Models;
+
+namespace LeagueManagerPost.Controllers
+{
+    public class GamesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.AwayTeam).Include(g => g.HomeTeam);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.AwayTeamId = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.HomeTeamId = new SelectList(db.Teams, "Id", "Name");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,HomeTeamId,AwayTeamId")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AwayTeamId = new SelectList(db.Teams, "Id", "Name", game.AwayTeamId);
+            ViewBag.HomeTeamId = new SelectList(db.Teams, "Id", "Name", game.HomeTeamId);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AwayTeamId = new SelectList(db.Teams, "Id", "Name", game.AwayTeamId);
+            ViewBag.HomeTeamId = new SelectList(db.Teams, "Id", "Name", game.HomeTeamId);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,HomeTeamId,AwayTeamId")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AwayTeamId = new SelectList(db.Teams, "Id", "Name", game.AwayTeamId);
+            ViewBag.HomeTeamId = new SelectList(db.Teams, "Id", "Name", game.HomeTeamId);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LeagueManagerPost/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeagueManagerPost.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public Team HomeTeam { get; set; }
+        public int HomeTeamId { get; set; }
+
+        public Team AwayTeam { get; set; }
+        public int AwayTeamId { get; set; }
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
